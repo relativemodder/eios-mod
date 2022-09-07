5126707efe6bfe754911c2394ae2637e60c7b3bb
Andrey <61388196+relativemodder@users.noreply.github.com>
2022-09-07 15:01:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ class EiosCustomiser{
       var urlRegex = /(https?:\/\/[^\s]+)/g;
       return text.replace(urlRegex, function(url) {
         	
-          return '<a href="' + url + '">' + url + '</a>';
+          return '<a href="' + url + '" target="_blank">' + url + '</a>';
       })
       // or alternatively
       // return text.replace(urlRegex, '<a href="$1">$1</a>')
